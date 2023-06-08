232ed67598816a17d2889d6055d6fa68262135d0
AlexSoft <4276038@gmail.com>
2023-06-08 18:11:33 +0300

    task 1 added

---
+++ developer/exception-in-programming/homeworks/hw2/task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿double? PromptDouble(string intro, bool oneline = true)
+{   
+    double? res = null;
+    if (intro != null) Console.Write($"{intro}" + ((oneline) ? "" : "\n").ToString());
+    try
+    {
+        return double.Parse(Console.ReadLine()!);
+    }
+    catch (System.FormatException)
+    {
+        Console.WriteLine("Нельзя вводить строку. Повторите ввод!");
+    }
+    return res;
+}
+
+double? result = null;
+while (result == null)
+    result = PromptDouble("Введите дробное число:");
+
+Console.WriteLine($"Вы ввели: {result}");
