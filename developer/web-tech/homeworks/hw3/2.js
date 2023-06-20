dab22ceb15a4c4b2abe28cf121e0e373980c9799
AlexSoft <4276038@gmail.com>
2023-06-21 00:46:07 +0300

    correction hw3

--- developer/web-tech/homeworks/hw3/2.js
+++ developer/web-tech/homeworks/hw3/2.js
@@ -1,11 +1,11 @@
 // Запрашиваем имя 
 function getName() {
-    let user_name = prompt('Введите Ваше имя:');
-    grettings(user_name);
+    const userMame = prompt('Введите Ваше имя:');
+    grettings(userName);
 }
 // Выводим приветсвтие
 function grettings(name) {
-    msg = 'Привет, ' + name;
+    const msg = 'Привет, ' + name;
     alert(msg); // выводи через всплывающее окно для проверки
     console.log(msg); // вывородим в консоль по заданию
 }
