f36e83d9eea8c5186ccc0ad1700c8609fc8a9bfb
AlexSoft <4276038@gmail.com>
2023-07-15 13:49:32 +0300

    Добавлена 5 домашка

--- tester/java-basics/homework_5/homework_5.java
+++ tester/java-basics/homework_5/homework_5.java
@@ -1,5 +1,6 @@
 package homework_5;
 // Реализуйте структуру телефонной книги с помощью HashMap.
+
 // Программа также должна учитывать, 
 // что во входной структуре будут повторяющиеся имена с разными телефонами, 
 // их необходимо считать, как одного человека с разными телефонами. 
@@ -9,41 +10,85 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Scanner;
+
 public class homework_5 {
+    public static HashMap<String, ArrayList<String>> phoneBook = new HashMap<>();
+    public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        HashMap<String, ArrayList<String>> phoneBook = GetPhoneBook();
-        System.out.println("Исходный книга: ");
-        System.out.println(phoneBook);
-        PrintPhoneBook(phoneBook);
-    }
+        HashMap<String, ArrayList<String>> phoneFind;
 
-    public static HashMap<String, ArrayList<String>> GetPhoneBook() {
-        Scanner scanner = new Scanner(System.in);
-        HashMap<String, ArrayList<String>> phoneBook = new HashMap<>();
-        System.out.println("Вводите имена и телефоны, чтобы выйти вместо имени введите пустую строку.");
         while (true) {
-            System.out.print("Введите имя: ");
-            String name = scanner.nextLine();
-            if (name.equals(""))
-                break;
-            System.out.print("Введите телефон: ");
-            String phone = scanner.nextLine();
-            ArrayList<String> tmp = new ArrayList<>();
-            if (phoneBook.containsKey(name)) {
-                tmp = phoneBook.get(name);
+            System.out.println("Команды: ");
+            System.out.println("1 - Добавить запись");
+            System.out.println("2 - Найти запись");
+            System.out.println("3 - Показать всю книгу");
+            System.out.println("0 - Выход");
+            System.out.print("Ваша команда: ");
+            String key = scanner.nextLine();
+
+            switch (key) {
+                case "1":
+                    AddPBRecord();
+                    break;
+                case "2":
+                    phoneFind = GetPBRecord();
+                    if (phoneFind != null)
+                        PrintPB(phoneFind);
+                    else
+                        System.out.println("Запись не найдена!");
+                    break;
+                case "3":
+                    PrintPB(phoneBook);
+                    break;
+
+                default:
+                    scanner.close();
+                    return;
             }
-            tmp.add(phone);
-            phoneBook.put(name, tmp);
         }
-        scanner.close();
-        return phoneBook;
     }
 
-    public static void PrintPhoneBook(HashMap<String, ArrayList<String>> phoneBook) {
+    public static boolean AddPBRecord() {
+
+        System.out.println("Вводите имена и телефоны");
+
+        System.out.print("Введите имя: ");
+        String name = scanner.nextLine();
+        if (name.equals(""))
+            return false;
+        System.out.print("Введите телефон: ");
+        String phone = scanner.nextLine();
+        ArrayList<String> tmp = new ArrayList<>();
+        if (phoneBook.containsKey(name)) {
+            tmp = phoneBook.get(name);
+        }
+        tmp.add(phone);
+        phoneBook.put(name, tmp);
+        return true;
+    }
+
+    public static HashMap<String, ArrayList<String>> GetPBRecord() {
+        HashMap<String, ArrayList<String>> result = new HashMap<>();
+        System.out.print("Введите имя для поиска: ");
+        String name = scanner.nextLine();
+        if (name.equals(""))
+            return null;
+        if (phoneBook.containsKey(name))
+            result.put(name, phoneBook.get(name));
+        else
+            result = null;
+        return result;
+    }
+
+    public static void PrintPB(HashMap<String, ArrayList<String>> pb) {
         ArrayList<String> keys = new ArrayList<>();
-        phoneBook.keySet().forEach(v -> keys.add(v));
+        pb.keySet().forEach(v -> keys.add(v));
+        // Добавлена сортировка было на другом уроке не стал убирать, в приницпе здесь не нужна
         Collections.sort(keys, (o1, o2) -> phoneBook.get(o2).size() - phoneBook.get(o1).size());
+        System.out.println("====================");
         System.out.println("Фамилия : Телефоны");
-        keys.forEach(v -> System.out.println(v+" : "+phoneBook.get(v)));
+        keys.forEach(v -> System.out.println(v + " : " + pb.get(v)));
+        System.out.println("====================");
     }
 }
